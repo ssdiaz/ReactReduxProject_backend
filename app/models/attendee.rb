342dc83f747cf095366f484540d17d4c30414fab
Samantha Diaz <samidiaz3@gmail.com>
2022-04-01 10:57:24 -0400

    also commented out validates :phone in attendee model- this removed the requirment when making a new attendee

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,9 +1,10 @@
 class Attendee < ApplicationRecord
     belongs_to :trip
 
-    validates_presence_of :name, :phone, :relationship
+    validates_presence_of :name, :relationship
+    # validates_presence_of :name, :phone, :relationship
    
-    validates :phone, numericality: { only_integer: true }, length: { is: 10 }
+    # validates :phone, numericality: { only_integer: true }, length: { is: 10 }
 
     validates_inclusion_of :status, in: ['', 'Confirmed', 'Maybe', 'Not Coming']
     validates_inclusion_of :relationship, in: ['Bride', 'Bridesmaid', 'Maid of Honor', 'Matron of Honor', 'Attendee']
